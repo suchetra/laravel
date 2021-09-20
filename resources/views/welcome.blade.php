d828099352eff5bd5dc0d2398600508202609aec
suchetra <nicolas.tan@le-campus-numerique.fr>
2021-09-20 11:03:49 +0200

    routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,8 @@
         </style>
     </head>
     <body class="antialiased">
-        <h1>Hello World From Laravel v8.X (PHP v7.4.X)</h1>
+        <h1>Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+</h1>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 2e parametre est une fonction anonyme qui est une classe closure
+// Classe utilisée pour représenter les fonctions anonymes.
+// Les fonctions anonymes produisent des objets de ce type.
+// get est une méthode pour intéragir avec l'url
+
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return "je suis dans la route produit 1";
+// });
+
+Route::get('1', function() { return 'Je suis la page 1 !'; });
+Route::get('2', function() { return 'Je suis la page 2 !'; });
+Route::get('3', function() { return 'Je suis la page 3 !'; });
